53b7f3d8dfc8708cbf21aee0e8e6c0ddbdaba462
dylanmitchell1007 <armadummy@yahoo.com>
2017-03-29 18:06:15 -0500

    3/29/17

--- MathLib.py
+++ MathLib.py
@@ -1,56 +1,27 @@
+import math
+
 class Vector2(object):
 
-    def __init__(self, VecX, VecY):
+    def __init__(self, xpos, ypos):
         self.VecX = xpos
         self.VecY = ypos
+    def __add__(self, other):
+        return Vector2 (self.VecX + other.VecX, self.VecY + other.VecY)
+    def Magnitude( self):
+        return math.sqrt((self.VecX * self.VecX) + (self.VecY * self.VecY))
 
-    def Add(self, Vec):        
-        x = self.VecX + Vec2.xx
-        y = self.VecY + Vec2.yy
-        return Vector2(x, y)        
+    def Normalize(self):
+        return Vector2(self.VecX  / self.Magnitude(), self.VecY / self.Magnitude())
 
-    def Sub(self, sub, Vec):
-        x = self.VecX - Vec.xx
-        y = self.VecY - Vec.yy
-        return Vector2(x, y)
+    def DotProd(self, dotprod):
+        return (self.VecX * self.VecX) + (self.VecY * self.VecY)
 
-    def Mult(self, mult, Vec):
-        x = self.VecX * Vec.xx
-        x = self.VecY * Vec.yy
+    def GetY(self):
+        return self.VecY
+    def GetX(self):
+        return self.VecX
 
-    def AddConst(self, ADDCONST):
-        Vec2 = work
-        work.xx = self.VecX * mult
-        work.yy = self.VecY * mult
-        return work
-
-    def SubConst(self, SUBCONST):
-        Vec2 = work
-        work.xx = self.VecX - sub.xx
-        work.yy = self.VecY - sub.yy
-        return work
-
-    def MultConst(self, MULTCONST):
-        Vec2 = work
-        work.xx = self.VecX * mult
-        work.yy = self.VecY * mult
-        return work
-
-    def Vec2(self, equals):
-        return self.VecX = self.VecX, self.VecY = self.VecY
-
-    def Magnitude(Magnitude):
-        return sqrt((VecX * VecX) + (VecY * VecY))
-
-    def Normalize(Normalize):
-        return Vec2(Vec2.xx / Magnitude(), Vec2.yy / Magnitude())
-
-    def DotProd(dotprod):
-        return (Vec2.xx * Vec2.xx) + (Vec2.yy * Vec2.yy)
-
-    def GetY():
-        return Vec2.yy
-        print Vector2.GetY()
-    def GetX():
-        return Vec2.xx
-        print Vector2.GetX()
+test = Vector2(0, 2)
+test2 = Vector2(1,1)
+Sum = test + test2
+print Sum
